e47c8adad22b0fb1a61ec69712feff26ce88c4dd
umedha33 <umedharaj@gmail.com>
2024-02-06 13:48:42 +0530

    stopwatch

--- index.js
+++ index.js
@@ -2003,3 +2003,55 @@
 
 // livetime();
 // setInterval(livetime, 1000);
+
+
+
+// stop watch ---------------------------------------------------------------------------------
+
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+
+function start() {
+    if (!isRunning) {
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+}
+
+function stop() {
+    if (isRunning) {
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+}
+
+function reset() {
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    display.textContent = "00:00:00:00";
+    isRunning = false;
+}
+
+function update() {
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = hours.toString().padStart(2, "0");
+    minutes = minutes.toString().padStart(2, "0");
+    seconds = seconds.toString().padStart(2, "0");
+    milliseconds = milliseconds.toString().padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`
+}
