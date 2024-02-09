ac055b3c47c3a4cc79c4d1874d7bbb2527146672
umedha33 <umedharaj@gmail.com>
2024-02-09 14:06:45 +0530

    calculator program

--- index.js
+++ index.js
@@ -2111,3 +2111,27 @@
 // }
 
 // console.log("reached the end");
+
+
+
+// Calculator program -------------------------------------------------------------------------------------------------------
+
+// const display = document.getElementById("display");
+
+// function appendToDisplay(input) {
+//     display.value += input;
+// }
+
+// function clearDisplay() {
+//     display.value = "";
+// }
+
+// function calculate() {
+//     try {
+//         display.value = eval(display.value);
+//     }
+
+//     catch (error) {
+//         display.value = "ERROR";
+//     }
+// }
