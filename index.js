66b048aa221d6c318b35215cb698fa118c8dce4d
umedha33 <umedharaj@gmail.com>
2024-02-10 23:00:02 +0530

    add/change html elem

--- index.js
+++ index.js
@@ -2272,3 +2272,28 @@
 // const children = element.children;
 
 // children[2].style.background = "blue";
+
+
+
+// Add and change html elements using Js -------------------------------------------------------------------------------------------------------
+
+// step 1 - create the element...
+// const newH1 = document.createElement("h1");
+
+// // step 2 - add attributes/properties...
+// newH1.textContent = "I like Sony";
+// newH1.id = "newh1";
+// newH1.style.color = "tomato";
+// newH1.style.textAlign = "center";
+
+// // step 3 - append element to DOM...
+// // document.body.append(newH1);
+// // document.body.prepend(newH1);
+// document.getElementById("box1").append(newH1);
+// // document.getElementById("box1").prepend(newH1);
+
+// // const box2 = document.getElementById("box2");
+// // document.body.insertBefore(newH1, box2);
+
+// // remove html elements...
+// document.getElementById("box1").removeChild(newH1);
